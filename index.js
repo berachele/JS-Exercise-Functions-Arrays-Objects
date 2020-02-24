e923b549cb4f74eca14d2bfb7bdd1f4f98031785
Rachele Edwards <dot.com1472@gmail.com>
2020-02-24 15:09:18 -0700

    FINALLY got the makeSmartPerson--arrrgh instructions X{

--- index.js
+++ index.js
@@ -73,9 +73,18 @@ function getName(name) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
-}
+function makeSmartPerson(name) {
+  const smartPerson = {
+    name: name,
+    sum: function(num1, num2){
+      return num1 + num2;
+    },//end of sum method
+    speak: function(){
+      return "Hello, my name is " + name;
+    }
+  }//end of smartPerson
+  return smartPerson;
+}//end of function
 
 
 
